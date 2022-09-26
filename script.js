c93e6febe1316d3ca00c0d2ac160c490ca05e16a
Daniela3006 <91320359+Daniela3006@users.noreply.github.com>
2022-09-26 17:10:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ for (let y = 18; y > 0; y--) {
     }
 }
 
-let x = 5, y = 15;
+let x = 5, y = 15;//ось x y
+
+//фигурки 
 let mainArr = [
     //палка
     [
@@ -95,25 +97,33 @@ function create() {
         figureBody[i].classList.add('figure');
     }
 } create();
+//функция падения фигур
 function move() {
-    let moveFlag = true;
+    let moveFlag = true; //если перемен=true => фигурка на один ряд вниз
+
+    //получаем координаты(их 4 у всех фигур) каждой ячейки из фигурки 
     let coordinates = [
-        [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')],
-        [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')],
-        [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')],
-        [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')],
+        [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')],// 5 по оси х и 15 по оси y
+        [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')],// 5 по оси х и 16 по оси y
+        [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')],// 5 по оси х и 17 по оси y
+        [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')],// 5 по оси х и 18 по оси y
     ];
+
+    //проверяем если фигурка может падать дальше или нет
+
+    //фигурка на последнем ряду||ниже есть упавшая фигурка(все двигающиеся = класс figureб останов = класс set)
     for (let i = 0; i < coordinates.length; i++) {
         if (coordinates[i][1] == 1 || document.querySelector(`[posX="${coordinates[i][0]}"][posY="${coordinates[i][1] - 1}"]`).classList.contains('set')) {
             moveFlag = false;
             break;
         }
     }
-
-    if (moveFlag) {
+    //  убираем класс figure
+    if (moveFlag == true) {
         for (let i = 0; i < figureBody.length; i++) {
             figureBody[i].classList.remove('figure');
         }
+        //понижаем ряд на -1 поз х не меняетсяя, только поз y
         figureBody = [
             document.querySelector(`[posX="${coordinates[0][0]}"][posY="${coordinates[0][1] - 1}"]`),
             document.querySelector(`[posX="${coordinates[1][0]}"][posY="${coordinates[1][1] - 1}"]`),
@@ -121,40 +131,64 @@ function move() {
             document.querySelector(`[posX="${coordinates[3][0]}"][posY="${coordinates[3][1] - 1}"]`),
 
         ];
+
+        //добовляем класс figurе обрано
         for (let i = 0; i < figureBody.length; i++) {
             figureBody[i].classList.add('figure');
         }
-    } else {
+    }
+    //если moveFlag==false то меняем класс на set
+    else {
         for (let i = 0; i < figureBody.length; i++) {
             figureBody[i].classList.remove('figure');
             figureBody[i].classList.add('set');
         }
-        create();
+
+        create();//создаем новую фигуру
     }
 }
-let interval = setInterval(() => { move(); }, 300);
-let flag = true;
 
+//запустили интервал каторый повторит функю move каждые 300мс
+let interval = setInterval(() => { move(); }, 300);
+
+
+
+
+//до этого момента фигурки просто падают вниз по серед. без остановки друг на друга пока не закончится пространство по оси y
+
+
+
+
+let flag = true; //вспомог переменная ниже
+
+
+// подключаем стрелки 
 window.addEventListener('keydown', function (e) {
     let coordinates1 = [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')];
     let coordinates2 = [figureBody[1].getAttribute('posX'), figureBody[2].getAttribute('posY')];
     let coordinates3 = [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')];
     let coordinates4 = [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')];
-
+//определяем новое полож фигурки в пространстве
     function getNewState(a) {
         flag = true;
         let figureNew = [
-            document.querySelector(`[posX="${+coordinates1[0] + a}"][posY="${coordinates1[1] }"]`),
-            document.querySelector(`[posX="${+coordinates2[0] + a}"][posY="${coordinates2[1] }"]`),
-            document.querySelector(`[posX="${+coordinates3[0] + a}"][posY="${coordinates3[1] }"]`),
-            document.querySelector(`[posX="${+coordinates4[0] + a}"][posY="${coordinates4[1] }"]`),
+            document.querySelector(`[posX="${+coordinates1[0] + a}"][posY="${coordinates1[1]}"]`),
+            document.querySelector(`[posX="${+coordinates2[0] + a}"][posY="${coordinates2[1]}"]`),
+            document.querySelector(`[posX="${+coordinates3[0] + a}"][posY="${coordinates3[1]}"]`),
+            document.querySelector(`[posX="${+coordinates4[0] + a}"][posY="${coordinates4[1]}"]`),
         ];
 
+
         for (let i = 0; i < figureNew.lengthl; i++) {
+
+            //если figureNew не существ || или содержит класс set flag=false=> при нажатии ничего не произ.
             if (!figureNew[i] || figureNew[i].classList.contains('set')) {
                 flag = false;
             }
         }
+
+
+          //если figureNew  существ то убираем класс figure  и перезапис коордонаты в класс figureNew  затем перезапис нов коорд в класс figure
         if (flag == true) {
             for (let i = 0; i < figureBody.length; i++) {
                 figureBody[i].classList.remove('figure');
@@ -166,13 +200,17 @@ window.addEventListener('keydown', function (e) {
             }
         }
     }
+    //то что произайдет при нажатии
     if (e.key == "ArrowLeft") {
-        getNewState(-1);
+        getNewState(-1);//в функц гетню прибав (-1)
     } else if (e.key == "ArrowRight") {
-        getNewState(1);
+        getNewState(1);//в функц гетню прибав (1)
     }
+    //ускорение падения (но пч то оно не работает)
     else if (e.keye == "ArrowDown") {
         move();
     }
 
 })
+
+// ПОМОГИ ПОЗЯЯ!   ^_^
